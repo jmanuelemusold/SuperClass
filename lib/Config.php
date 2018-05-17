ba9666e853ff09d8b4ef223ef5857a0a3e4f7401
Juan Manuel <hola@jmanuelem.us>
2018-05-17 12:29:19 -0600

    Added trait Config to autoload settings from config files

---
+++ lib/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+trait Config
+{
+   /**
+    * @var array<key, value>
+    */ 
+    private static $__config;
+
+   /**
+    * @return mixed
+    */
+    protected static function config(...$args)
+    {
+        $cfg = self::$__config;
+
+        // TODO
+    }
+}
